31b08a5ca799d6b21a2a41c8c2af097b5ae2e9d6
amalshaji <amalshajid@gmail.com>
2020-07-05 11:55:59 +0530

    origin master

---
+++ pyga/__init__.py
@@ -0,0 +1,4 @@
+from pyga.population import *
+from pyga.selection import *
+from pyga.genetic import *
+from pyga.crossover import *
---
+++ pyga/genetic.py
@@ -0,0 +1,21 @@
+from tqdm import tqdm
+
+class GeneticAlgorithm:
+    def __init__(self, population, objective_fn, selection, crossover) -> None:
+        self.population = population
+        self.objective_fn = objective_fn
+        self.selection = selection
+        self.crossover = crossover
+
+    def forward(self, iterations):
+        objective, pop = None, None
+        pop = self.population
+
+        for i in tqdm(range(iterations), ncols=100):
+            objective = self.objective_fn(*pop.T)
+            fitness = 1/(1+objective)
+            pop = self.selection(self, pop, fitness)
+            pop = self.crossover(self, pop, fitness)
+            if (objective == 0).sum() >= 1:
+                break
+        return iterations, objective, pop
