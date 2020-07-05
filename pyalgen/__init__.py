e40c8892b60f70b4315a78709a882f59c837f494
amalshaji <amalshajid@gmail.com>
2020-07-05 14:59:09 +0530

    changed test functions import

--- pyalgen/__init__.py
+++ pyalgen/__init__.py
@@ -3,4 +3,4 @@ from pyalgen.selection import *
 from pyalgen.genetic import *
 from pyalgen.crossover import *
 from pyalgen.mutation import *
-from pyalgen.test.test_functions import *
+from pyalgen.test_functions import *
