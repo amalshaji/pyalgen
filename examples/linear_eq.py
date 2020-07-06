9573bb52b13b34ccc48d7c6694ad0d33e5ef746e
amalshaji <amalshajid@gmail.com>
2020-07-06 13:13:39 +0530

    minor changes

--- examples/linear_eq.py
+++ examples/linear_eq.py
@@ -1,14 +1,16 @@
 import pyalgen
 
-pop = pyalgen.Population(1, 30, unique=True, dtype='int')
+pop = pyalgen.Population(1, 30, unique=True, dtype="int")
 population = pop(1000, 4)
 # print(population)
 selection = pyalgen.Selection.tournament
 crossover = pyalgen.Crossover.onepoint
 mutation = pyalgen.Mutation.randompoint
 
+
 def obj(a, b, c, d):
-    return a + 2*b + 3*c + 4*d - 30
+    return a + 2 * b + 3 * c + 4 * d - 30
+
 
 ga = pyalgen.GeneticAlgorithm(population, obj, selection, crossover, mutation)
 
@@ -16,6 +18,10 @@ ga = pyalgen.GeneticAlgorithm(population, obj, selection, crossover, mutation)
 iterations, objective, pop = ga.forward(iterations=1000)
 
 if iterations == 1000:
-    print(f'min_value: {objective.min()}, solution: {pop[objective.argmin()]}, generation: {iterations}')   
+    print(
+        f"min_value: {objective.min()}, solution: {pop[objective.argmin()]}, generation: {iterations}"
+    )
 else:
-    print(f'min_value: {objective[objective == 0][0]}, solution: {pop[objective == 0][0]}, generation: {iterations}')   
+    print(
+        f"min_value: {objective[objective == 0][0]}, solution: {pop[objective == 0][0]}, generation: {iterations}"
+    )
--- pyalgen/genetic.py
+++ pyalgen/genetic.py
@@ -1,8 +1,14 @@
 import numpy as np
 from tqdm import tqdm
 
+
 class GeneticAlgorithm:
-    def __init__(self, population, objective_fn, selection, crossover, mutation) -> None:
+    def __init__(
+        self, population, objective_fn, selection, crossover, mutation
+    ) -> None:
+        """Class combines various objects like population, selection, crossover and mutation 
+        to create a GA object and perform operation
+        """
         self.population = population
         self.objective_fn = objective_fn
         self.selection = selection
@@ -20,5 +26,10 @@ class GeneticAlgorithm:
             fitness = 1 / objective
             pop = self.selection(pop, fitness)
             pop = self.crossover(pop, fitness)
-            pop = self.mutation(pop, low=np.min(self.population), high=np.max(self.population), dtype=self.population[-1][-1].dtype)
+            pop = self.mutation(
+                pop,
+                low=np.min(self.population),
+                high=np.max(self.population),
+                dtype=self.population[-1][-1].dtype,
+            )
         return i, objective, pop
